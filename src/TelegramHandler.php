e0062ead972e5e16e8c0a923bdd20a0bdd8c2428
Mponos George <gmponos@gmail.com>
2020-06-10 23:22:23 +0300

    Setup the ci tools

--- src/TelegramHandler.php
+++ src/TelegramHandler.php
@@ -5,6 +5,9 @@ declare(strict_types=1);
 namespace MonologHttp;
 
 use Monolog\Logger;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
 
 /**
  * Logs to Cube.
@@ -13,5 +16,18 @@ use Monolog\Logger;
  */
 final class TelegramHandler extends AbstractHttpClientHandler
 {
+    public function __construct(
+        ClientInterface $client,
+        RequestFactoryInterface $requestFactory,
+        $level = Logger::ERROR,
+        bool $bubble = true
+    ) {
+        parent::__construct($client, $requestFactory, $level, $bubble);
+    }
+
     // todo
+    public function createRequest(array $record): RequestInterface
+    {
+        return $this->requestFactory->createRequest('GET', 'what');
+    }
 }
