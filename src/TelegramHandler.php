8a67ec2866f89bbe28b7cdab76a9c155e6d396f5
Savvas Alexandrou <savvas.alexandrou@gmail.com>
2020-06-11 12:53:24 +0200

    '#3 Implement telegram handler'
    Try a different phpcs

--- src/TelegramHandler.php
+++ src/TelegramHandler.php
@@ -8,26 +8,64 @@ use Monolog\Logger;
 use Psr\Http\Client\ClientInterface;
 use Psr\Http\Message\RequestFactoryInterface;
 use Psr\Http\Message\RequestInterface;
+use InvalidArgumentException;
 
 /**
- * Logs to Cube.
+ * Logs to Telegram.
  *
- * @link http://square.github.com/cube/
  */
 final class TelegramHandler extends AbstractHttpClientHandler
 {
+    private const TELEGRAM_API = 'https://api.telegram.org/bot';
+
+    private $apiKey;
+
+    private $chatId;
+
+    /**
+     * @param string $apiKey
+     * @param int|string $chatId
+     * @param ClientInterface $client
+     * @param RequestFactoryInterface $requestFactory
+     * @param int $level
+     * @param bool $bubble
+     */
     public function __construct(
+        string $apiKey,
+        $chatId,
         ClientInterface $client,
         RequestFactoryInterface $requestFactory,
         $level = Logger::ERROR,
         bool $bubble = true
     ) {
+        $this->apiKey = $apiKey;
+        $this->chatId = $chatId;
         parent::__construct($client, $requestFactory, $level, $bubble);
     }
 
-    // todo
+    /**
+     * Create request to @link https://api.telegram.org/bot on SendMessage action
+     * @see https://core.telegram.org/bots/api#sendmessage
+     *
+     * @param array $record
+     * @return RequestInterface
+     */
     public function createRequest(array $record): RequestInterface
     {
-        return $this->requestFactory->createRequest('GET', 'what');
+        $uri = sprintf('%s%s%s', self::TELEGRAM_API, $this->apiKey, '/sendMessage');
+        $request = $this->requestFactory->createRequest('POST', $uri)->withHeader('Content-Type', ['application/json']);
+        $body = [
+            'chat_id' => $this->chatId,
+            'text' => $record['formatted'],
+        ];
+        $jsonBody = json_encode($body);
+        if (false === $jsonBody) {
+            throw new InvalidArgumentException('Could not format record to json');
+        }
+
+        $request->getBody()->write($jsonBody);
+        $request->getBody()->rewind();
+
+        return $request;
     }
 }
---
+++ tests/TelegramHandlerTest.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace MonologHttp\Tests;
+
+use MonologHttp\TelegramHandler;
+use PHPUnit\Framework\MockObject\MockObject;
+use PHPUnit\Framework\TestCase;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\StreamInterface;
+
+class TelegramHandlerTest extends TestCase
+{
+    public function testCreateRequest()
+    {
+        /** @var ClientInterface | MockObject $mockClient */
+        $mockClient = $this->getMockBuilder(ClientInterface::class)->getMock();
+        /** @var RequestFactoryInterface | MockObject $mockRequestFactory */
+        $mockRequestFactory = $this->getMockBuilder(RequestFactoryInterface::class)
+            ->getMock();
+        $mockBody = $this->getMockBuilder(StreamInterface::class)->getMock();
+        $mockBody->expects($this->once())
+            ->method('write')
+            ->with('{"chat_id":1234,"text":"This is an error message"}');
+        $mockBody->expects($this->once())->method('rewind');
+        $mockRequest = $this->getMockBuilder(RequestInterface::class)->getMock();
+        $mockRequest->expects($this->exactly(2))->method('getBody')->willReturn($mockBody);
+        $mockRequestFactory->expects($this->once())
+            ->method('createRequest')
+            ->with('POST', 'https://api.telegram.org/botTelegramApiKey/sendMessage')
+            ->willReturn($mockRequest);
+        $mockRequest->expects($this->once())
+            ->method('withHeader')
+            ->with('Content-Type', ['application/json'])
+            ->willReturn($mockRequest);
+
+        $telegramHandler = new TelegramHandler(
+            'TelegramApiKey',
+            1234,
+            $mockClient,
+            $mockRequestFactory
+        );
+        $actualRequest = $telegramHandler->createRequest(
+            ['formatted' => 'This is an error message']
+        );
+
+        $this->assertInstanceOf(RequestInterface::class, $actualRequest);
+    }
+}
