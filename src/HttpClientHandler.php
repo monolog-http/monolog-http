93b940ecee907b277c5f09fbe9d638a7fa243ab6
Mponos George <gmponos@gmail.com>
2020-06-26 23:15:45 +0300

    Added a Generic handler

---
+++ src/HttpClientHandler.php
@@ -0,0 +1,45 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MonologHttp;
+
+use Monolog\Logger;
+use Psr\Http\Client\ClientInterface;
+use Psr\Http\Message\RequestFactoryInterface;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\UriInterface;
+
+/**
+ * This is a generic purpose handler
+ */
+final class HttpClientHandler extends AbstractHttpClientHandler
+{
+    /**
+     * @var string|UriInterface
+     */
+    private $uri;
+
+    /**
+     * @var string
+     */
+    private $method;
+
+    public function __construct(
+        ClientInterface $client,
+        RequestFactoryInterface $requestFactory,
+        $uri,
+        string $method,
+        $level = Logger::ERROR,
+        bool $bubble = true
+    ) {
+        parent::__construct($client, $requestFactory, $level, $bubble);
+        $this->uri = $uri;
+        $this->method = $method;
+    }
+
+    protected function createRequest(array $record): RequestInterface
+    {
+        return $this->requestFactory->createRequest($this->method, $this->uri);
+    }
+}
